00693afbfe01f804ce18083f14a357b6bc7672fc
Lea Pertzborn <rohlfs@cip.ifi.lmu.de>
2023-11-22 22:35:22 +0100

    Bugfix Filter

--- data.js
+++ data.js
@@ -26,17 +26,21 @@ function changeImage(imageSrc) {
     displayedImage.src = imageSrc;
 }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+document.addEventListener("DOMContentLoaded", function() {
+  var acc = document.getElementsByClassName("accordion-button");
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+  for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+});
+
+
